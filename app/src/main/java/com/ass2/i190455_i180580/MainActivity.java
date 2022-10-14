3c62175e808e7b42e1a354007aaf28d76f2ae92e
usman182 <i190455@nu.edu.pk>
2022-10-14 23:31:36 +0500

    Commited

--- app/src/main/java/com/ass2/i190455_i180580/MainActivity.java
+++ app/src/main/java/com/ass2/i190455_i180580/MainActivity.java
@@ -2,13 +2,35 @@ package com.ass2.i190455_i180580;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
+    Timer timer;
+    ImageView img1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, activity2.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1500);
+
+
+
     }
 }
