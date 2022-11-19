02d480cc4e16dd5082bf9299e50d78ea55f67681
usman182 <i190455@nu.edu.pk>
2022-11-19 23:50:40 +0500

    Commited

--- app/src/main/java/com/ass2/i190455_i180580/MsgHome.java
+++ app/src/main/java/com/ass2/i190455_i180580/MsgHome.java
@@ -2,21 +2,35 @@ package com.ass2.i190455_i180580;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.ContextWrapper;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.ass2.i190455_i180580.ui.login.CreateContactFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.io.File;
+import java.io.IOException;
+
 public class MsgHome extends AppCompatActivity {
     BottomNavigationView bnv;
     ImageView add;
     FirebaseAuth mAuth;
+    MediaRecorder mediaRecorder;
+    MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +38,7 @@ public class MsgHome extends AppCompatActivity {
 
         bnv=findViewById(R.id.bnv);
         add=findViewById(R.id.add_bt);
+
 
         ChatFragment chatfrag=new ChatFragment();
         PhoneFragment phonefrag=new PhoneFragment();
@@ -65,7 +80,80 @@ public class MsgHome extends AppCompatActivity {
             }
         });
 
+        // if the microphone is present then call the getMicrophone permission
+        if (isMicrophonePresent()) {
+            getMicrophonePermission();
+        }
 
+
+    }
+
+
+    // when record button is pressed
+    public void btnRecordPressed(View v) {
+
+        try {
+            mediaRecorder = new MediaRecorder();
+            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mediaRecorder.setOutputFile(getRecordingFilePath());
+            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mediaRecorder.prepare();
+            mediaRecorder.start();
+
+            Toast.makeText(this, "Recorded Started", Toast.LENGTH_SHORT).show();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // when stop button is pressed
+    public void btnStopPressed(View v) {
+            mediaRecorder.stop();
+            mediaRecorder.release();
+            mediaRecorder = null;
+
+        Toast.makeText(this, "Recorded Stopped", Toast.LENGTH_SHORT).show();
+    }
+
+    // when play button is pressed
+    public void btnPlayPressed(View v) throws IOException {
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setDataSource(getRecordingFilePath());
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+
+        Toast.makeText(this, "Recorded Playing", Toast.LENGTH_SHORT).show();
+    }
+
+    // check for the microphone presence
+    private boolean isMicrophonePresent() {
+        if (this.getPackageManager().hasSystemFeature(PackageManager.FEATURE_MICROPHONE)) {
+            return true;
+        }
+
+        else {
+            return false;
+        }
+    }
+
+    // get the mircrophone permission
+    private void getMicrophonePermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+        == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, new String[]
+                    {Manifest.permission.RECORD_AUDIO}, 100);
+        }
+    }
+
+
+    // get the path for storing the audio file
+    private String getRecordingFilePath() {
+        ContextWrapper contextWrapper = new ContextWrapper(getApplicationContext());
+        File musicDirectory = contextWrapper.getExternalFilesDir(Environment.DIRECTORY_MUSIC);
+        File file = new File(musicDirectory, "abc" + ".mp3");
+        return file.getPath();
     }
 
 
